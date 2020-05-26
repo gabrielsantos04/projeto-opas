203fbe9ee9f18b4278373549b85e0913d3d55124
Gabriel <gabrielvash@gmail.com>
2020-05-26 15:15:29 -0300

    correcao dant responsaveis

--- app/controllers/dant_responsavel_programs_controller.rb
+++ app/controllers/dant_responsavel_programs_controller.rb
@@ -6,7 +6,7 @@ class DantResponsavelProgramsController < ApplicationController
   # GET /dant_responsavel_programs
   def index
     @cidades = Cidade.all.order(:nome).map{|a| [a.nome,a.id]}
-    if current_user.administrador? || current_user.admin_dant
+    if current_user.administrador? || current_user.admin_dant?
       @q = DantResponsavelProgram.all.ransack(params[:q])
     else
       @q = DantResponsavelProgram.where(cidade: current_user.cidade).ransack(params[:q])
