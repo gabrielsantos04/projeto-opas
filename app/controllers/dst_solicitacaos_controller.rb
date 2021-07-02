e22b029df173538edac5b54bfbda6b7b74ccc759
Gabriel Santos <gabriel.santos@datum.inf.br>
2021-07-02 14:26:29 -0300

    Exibe apenas solicitações autorizadas para o estoque

--- app/controllers/dst_solicitacaos_controller.rb
+++ app/controllers/dst_solicitacaos_controller.rb
@@ -5,8 +5,10 @@ class DstSolicitacaosController < ApplicationController
 
   # GET /dst_solicitacaos
   def index
-    if current_user.admin_dst? || current_user.administrador? || current_user.dst_produtos?
+    if current_user.admin_dst? || current_user.administrador?
       @q = DstSolicitacao.all.ransack(params[:q])
+    elsif current_user.dst_produtos?
+      @q = DstSolicitacao.where(status: :autorizado).ransack(params[:q])
     else
       @q = current_user.dst_solicitacaos.ransack(params[:q])
     end
